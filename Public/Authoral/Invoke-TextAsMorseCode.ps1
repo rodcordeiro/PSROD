31a36d779d30f1ae1f4e01cdd2e9edb3a470f6de
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-11-29 19:21:24 -0300

    fix invoke morse

--- Public/Authoral/Invoke-TextAsMorseCode.ps1
+++ Public/Authoral/Invoke-TextAsMorseCode.ps1
@@ -30,42 +30,42 @@ function Invoke-TextAsMorseCode {
         function Char2MorseCode {
             param([string]$Char)
             switch ($Char) {
-                'A' { dot; gap 2; dash; gap 4 }
-                'B' { dash; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-                'C' { dash; gap 2; dot; gap 2; dash; gap 2; dot; gap 4 }
-                'D' { dash; gap 2; dot; gap 2; dot; gap 4 }
-                'E' { dot; gap 4 }
-                'F' { dot; gap 2; dot; gap 2; dash; gap 2; dot; gap 4 }
-                'G' { dash; gap 2; dash; gap 2; dot; gap 4 }
-                'H' { dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-                'I' { dot; gap 2; dot; gap 4 }
-                'J' { dot; gap 2; dash; gap 2; dash; gap 2; dash; gap 4 }
-                'K' { dash; gap 2; dot; gap 2; dash; gap 4 }
-                'L' { dot; gap 2; dash; gap 2; dot; gap 2; dot; gap 4 }
-                'M' { dash; gap 2; dash; gap 4 }
-                'N' { dash; gap 2; dot; gap 4 }
-                'O' { dash; gap 2; dash; gap 2; dash; gap 4 }
-                'P' { dot; gap 2; dash; gap 2; dash; gap 2; dot; gap 4 }
-                'Q' { dash; gap 2; dash; gap 2; dot; gap 2; dash; gap 4 }
-                'R' { dot; gap 2; dash; gap 2; dot; gap 4 }
-                'S' { dot; gap 2; dot; gap 2; dot; gap 4 }
-                'T' { dash; gap 4 }
-                'U' { dot; gap 2; dot; gap 2; dash; gap 4 }
-                'V' { dot; gap 2; dot; gap 2; dot; gap 2; dash; gap 4 }
-                'W' { dot; gap 2; dash; gap 2; dash; gap 4 }
-                'X' { dash; gap 2; dot; gap 2; dot; gap 2; dash; gap 4 }
-                'Y' { dash; gap 2; dot; gap 2; dash; gap 2; dash; gap 4 }
-                'Z' { dash; gap 2; dash; gap 2; dot; gap 2; dot; gap 4 }
-                '1' { dot; gap 2; dash; gap 2; dash; gap 2; dash; gap 2; dash; gap 4 }
-                '2' { dot; gap 2; dot; gap 2; dash; gap 2; dash; gap 2; dash; gap 4 }
-                '3' { dot; gap 2; dot; gap 2; dot; gap 2; dash; gap 2; dash; gap 4 }
-                '4' { dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 2; dash; gap 4 }
-                '5' { dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-                '6' { dash; gap 2; dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-                '7' { dash; gap 2; dash; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-                '8' { dash; gap 2; dash; gap 2; dash; gap 2; dot; gap 2; dot; gap 4 }
-                '9' { dash; gap 2; dash; gap 2; dash; gap 2; dash; gap 2; dot; gap 4 }
-                '0' { dash; gap 2; dash; gap 2; dash; gap 2; dash; gap 2; dash; gap 4 }
+                'A' { dot; gap 1; dash; gap 2 }
+                'B' { dash; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+                'C' { dash; gap 1; dot; gap 1; dash; gap 1; dot; gap 2 }
+                'D' { dash; gap 1; dot; gap 1; dot; gap 2 }
+                'E' { dot; gap 2 }
+                'F' { dot; gap 1; dot; gap 1; dash; gap 1; dot; gap 2 }
+                'G' { dash; gap 1; dash; gap 1; dot; gap 2 }
+                'H' { dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+                'I' { dot; gap 1; dot; gap 2 }
+                'J' { dot; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
+                'K' { dash; gap 1; dot; gap 1; dash; gap 2 }
+                'L' { dot; gap 1; dash; gap 1; dot; gap 1; dot; gap 2 }
+                'M' { dash; gap 1; dash; gap 2 }
+                'N' { dash; gap 1; dot; gap 2 }
+                'O' { dash; gap 1; dash; gap 1; dash; gap 2 }
+                'P' { dot; gap 1; dash; gap 1; dash; gap 1; dot; gap 2 }
+                'Q' { dash; gap 1; dash; gap 1; dot; gap 1; dash; gap 2 }
+                'R' { dot; gap 1; dash; gap 1; dot; gap 2 }
+                'S' { dot; gap 1; dot; gap 1; dot; gap 2 }
+                'T' { dash; gap 2 }
+                'U' { dot; gap 1; dot; gap 1; dash; gap 2 }
+                'V' { dot; gap 1; dot; gap 1; dot; gap 1; dash; gap 2 }
+                'W' { dot; gap 1; dash; gap 1; dash; gap 2 }
+                'X' { dash; gap 1; dot; gap 1; dot; gap 1; dash; gap 2 }
+                'Y' { dash; gap 1; dot; gap 1; dash; gap 1; dash; gap 2 }
+                'Z' { dash; gap 1; dash; gap 1; dot; gap 1; dot; gap 2 }
+                '1' { dot; gap 1; dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
+                '2' { dot; gap 1; dot; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
+                '3' { dot; gap 1; dot; gap 1; dot; gap 1; dash; gap 1; dash; gap 2 }
+                '4' { dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 1; dash; gap 2 }
+                '5' { dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+                '6' { dash; gap 1; dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+                '7' { dash; gap 1; dash; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+                '8' { dash; gap 1; dash; gap 1; dash; gap 1; dot; gap 1; dot; gap 2 }
+                '9' { dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 1; dot; gap 2 }
+                '0' { dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
                 default { gap 7 } # medium gap (between words)
             }
         }
