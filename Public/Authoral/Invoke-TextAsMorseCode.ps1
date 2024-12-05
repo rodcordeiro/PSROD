a551f7540c621cb5e513648c5e96f6dbfef989a2
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2024-12-05 15:06:53 -0300

    fix: Import module issues

--- Public/Authoral/Invoke-TextAsMorseCode.ps1
+++ Public/Authoral/Invoke-TextAsMorseCode.ps1
@@ -10,7 +10,7 @@
         function gap {
             param([int]$Length)
             for ([int]$i = 1; $i -lt $Length; $i++) {
-                Write-Output " " -nonewline
+                Write-Output " " # -nonewline
                 continue;
             }
             start-sleep -milliseconds ($Length * $speed)
@@ -18,54 +18,54 @@
 
         function dot {
             [console]::beep(440, 150)
-            Write-Output "." -nonewline
+            Write-Output "." # -nonewline
             start-sleep -milliseconds $speed # signal
         }
 
         function dash {
             [console]::beep(440, 300)
-            Write-Output "_" -nonewline
+            Write-Output "_" # -nonewline
             start-sleep -milliseconds (3 * $speed) # signal
         }
         function Char2MorseCode {
             param([string]$Char)
             switch ($Char) {
-                'A' { dot; gap 1; dash; gap 2 }
-                'B' { dash; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
-                'C' { dash; gap 1; dot; gap 1; dash; gap 1; dot; gap 2 }
-                'D' { dash; gap 1; dot; gap 1; dot; gap 2 }
+                'A' { dot; dash; gap 2 }
+                'B' { dash; dot; dot; dot; gap 2 }
+                'C' { dash; dot; dash; dot; gap 2 }
+                'D' { dash; dot; dot; gap 2 }
                 'E' { dot; gap 2 }
-                'F' { dot; gap 1; dot; gap 1; dash; gap 1; dot; gap 2 }
-                'G' { dash; gap 1; dash; gap 1; dot; gap 2 }
-                'H' { dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
-                'I' { dot; gap 1; dot; gap 2 }
-                'J' { dot; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
-                'K' { dash; gap 1; dot; gap 1; dash; gap 2 }
-                'L' { dot; gap 1; dash; gap 1; dot; gap 1; dot; gap 2 }
-                'M' { dash; gap 1; dash; gap 2 }
-                'N' { dash; gap 1; dot; gap 2 }
-                'O' { dash; gap 1; dash; gap 1; dash; gap 2 }
-                'P' { dot; gap 1; dash; gap 1; dash; gap 1; dot; gap 2 }
-                'Q' { dash; gap 1; dash; gap 1; dot; gap 1; dash; gap 2 }
-                'R' { dot; gap 1; dash; gap 1; dot; gap 2 }
-                'S' { dot; gap 1; dot; gap 1; dot; gap 2 }
+                'F' { dot; dot; dash; dot; gap 2 }
+                'G' { dash; dash; dot; gap 2 }
+                'H' { dot; dot; dot; dot; gap 2 }
+                'I' { dot; dot; gap 2 }
+                'J' { dot; dash; dash; dash; gap 2 }
+                'K' { dash; dot; dash; gap 2 }
+                'L' { dot; dash; dot; dot; gap 2 }
+                'M' { dash; dash; gap 2 }
+                'N' { dash; dot; gap 2 }
+                'O' { dash; dash; dash; gap 2 }
+                'P' { dot; dash; dash; dot; gap 2 }
+                'Q' { dash; dash; dot; dash; gap 2 }
+                'R' { dot; dash; dot; gap 2 }
+                'S' { dot; dot; dot; gap 2 }
                 'T' { dash; gap 2 }
-                'U' { dot; gap 1; dot; gap 1; dash; gap 2 }
-                'V' { dot; gap 1; dot; gap 1; dot; gap 1; dash; gap 2 }
-                'W' { dot; gap 1; dash; gap 1; dash; gap 2 }
-                'X' { dash; gap 1; dot; gap 1; dot; gap 1; dash; gap 2 }
-                'Y' { dash; gap 1; dot; gap 1; dash; gap 1; dash; gap 2 }
-                'Z' { dash; gap 1; dash; gap 1; dot; gap 1; dot; gap 2 }
-                '1' { dot; gap 1; dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
-                '2' { dot; gap 1; dot; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
-                '3' { dot; gap 1; dot; gap 1; dot; gap 1; dash; gap 1; dash; gap 2 }
-                '4' { dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 1; dash; gap 2 }
-                '5' { dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
-                '6' { dash; gap 1; dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
-                '7' { dash; gap 1; dash; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
-                '8' { dash; gap 1; dash; gap 1; dash; gap 1; dot; gap 1; dot; gap 2 }
-                '9' { dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 1; dot; gap 2 }
-                '0' { dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
+                'U' { dot; dot; dash; gap 2 }
+                'V' { dot; dot; dot; dash; gap 2 }
+                'W' { dot; dash; dash; gap 2 }
+                'X' { dash; dot; dot; dash; gap 2 }
+                'Y' { dash; dot; dash; dash; gap 2 }
+                'Z' { dash; dash; dot; dot; gap 2 }
+                '1' { dot; dash; dash; dash; dash; gap 2 }
+                '2' { dot; dot; dash; dash; dash; gap 2 }
+                '3' { dot; dot; dot; dash; dash; gap 2 }
+                '4' { dot; dot; dot; dot; dash; gap 2 }
+                '5' { dot; dot; dot; dot; dot; gap 2 }
+                '6' { dash; dot; dot; dot; dot; gap 2 }
+                '7' { dash; dash; dot; dot; dot; gap 2 }
+                '8' { dash; dash; dash; dot; dot; gap 2 }
+                '9' { dash; dash; dash; dash; dot; gap 2 }
+                '0' { dash; dash; dash; dash; dash; gap 2 }
                 default { gap 7 } # medium gap (between words)
             }
         }
@@ -74,13 +74,13 @@
     process {
         try {
             if ($text -eq "" ) { [string]$text = read-host "Enter text to write" }
-
+            $response = ""
             [char[]]$ArrayOfChars = $text.ToUpper()
             foreach ($Char in $ArrayOfChars) {
-                Char2MorseCode $Char
+                $response += Char2MorseCode $Char
             }
             # exit 0 # success
-            Write-Output $ArrayOfChars
+            Write-Output $response
         }
         catch {
             throw "Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
--- Public/Imported/ConvertTo-MorseCode.ps1
+++ Public/Imported/ConvertTo-MorseCode.ps1
@@ -21,60 +21,61 @@
     function gap {
         param([int]$Length)
         for ([int]$i = 1; $i -lt $Length; $i++) {
-            write-host " " -nonewline
+            Write-Output " "# -nonewline
+            continue
         }
-        start-sleep -milliseconds ($Length * $speed)
+        # start-sleep -milliseconds ($Length * $speed)
     }
 
     function dot {
-        write-host "." -nonewline
-        start-sleep -milliseconds $speed # signal
+        Write-Output "." # -nonewline
+        # start-sleep -milliseconds $speed # signal
     }
 
     function dash {
-        write-host "_" -nonewline
-        start-sleep -milliseconds (3 * $speed) # signal
+        Write-Output "_" # -nonewline
+        # start-sleep -milliseconds (3 * $speed) # signal
     }
 
     function Char2MorseCode {
         param([string]$Char)
         switch ($Char) {
-            'A' { dot; gap 2; dash; gap 4 }
-            'B' { dash; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-            'C' { dash; gap 2; dot; gap 2; dash; gap 2; dot; gap 4 }
-            'D' { dash; gap 2; dot; gap 2; dot; gap 4 }
-            'E' { dot; gap 4 }
-            'F' { dot; gap 2; dot; gap 2; dash; gap 2; dot; gap 4 }
-            'G' { dash; gap 2; dash; gap 2; dot; gap 4 }
-            'H' { dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-            'I' { dot; gap 2; dot; gap 4 }
-            'J' { dot; gap 2; dash; gap 2; dash; gap 2; dash; gap 4 }
-            'K' { dash; gap 2; dot; gap 2; dash; gap 4 }
-            'L' { dot; gap 2; dash; gap 2; dot; gap 2; dot; gap 4 }
-            'M' { dash; gap 2; dash; gap 4 }
-            'N' { dash; gap 2; dot; gap 4 }
-            'O' { dash; gap 2; dash; gap 2; dash; gap 4 }
-            'P' { dot; gap 2; dash; gap 2; dash; gap 2; dot; gap 4 }
-            'Q' { dash; gap 2; dash; gap 2; dot; gap 2; dash; gap 4 }
-            'R' { dot; gap 2; dash; gap 2; dot; gap 4 }
-            'S' { dot; gap 2; dot; gap 2; dot; gap 4 }
-            'T' { dash; gap 4 }
-            'U' { dot; gap 2; dot; gap 2; dash; gap 4 }
-            'V' { dot; gap 2; dot; gap 2; dot; gap 2; dash; gap 4 }
-            'W' { dot; gap 2; dash; gap 2; dash; gap 4 }
-            'X' { dash; gap 2; dot; gap 2; dot; gap 2; dash; gap 4 }
-            'Y' { dash; gap 2; dot; gap 2; dash; gap 2; dash; gap 4 }
-            'Z' { dash; gap 2; dash; gap 2; dot; gap 2; dot; gap 4 }
-            '1' { dot; gap 2; dash; gap 2; dash; gap 2; dash; gap 2; dash; gap 4 }
-            '2' { dot; gap 2; dot; gap 2; dash; gap 2; dash; gap 2; dash; gap 4 }
-            '3' { dot; gap 2; dot; gap 2; dot; gap 2; dash; gap 2; dash; gap 4 }
-            '4' { dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 2; dash; gap 4 }
-            '5' { dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-            '6' { dash; gap 2; dot; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-            '7' { dash; gap 2; dash; gap 2; dot; gap 2; dot; gap 2; dot; gap 4 }
-            '8' { dash; gap 2; dash; gap 2; dash; gap 2; dot; gap 2; dot; gap 4 }
-            '9' { dash; gap 2; dash; gap 2; dash; gap 2; dash; gap 2; dot; gap 4 }
-            '0' { dash; gap 2; dash; gap 2; dash; gap 2; dash; gap 2; dash; gap 4 }
+            'A' { dot; gap 1; dash; gap 2 }
+            'B' { dash; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+            'C' { dash; gap 1; dot; gap 1; dash; gap 1; dot; gap 2 }
+            'D' { dash; gap 1; dot; gap 1; dot; gap 2 }
+            'E' { dot; gap 2 }
+            'F' { dot; gap 1; dot; gap 1; dash; gap 1; dot; gap 2 }
+            'G' { dash; gap 1; dash; gap 1; dot; gap 2 }
+            'H' { dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+            'I' { dot; gap 1; dot; gap 2 }
+            'J' { dot; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
+            'K' { dash; gap 1; dot; gap 1; dash; gap 2 }
+            'L' { dot; gap 1; dash; gap 1; dot; gap 1; dot; gap 2 }
+            'M' { dash; gap 1; dash; gap 2 }
+            'N' { dash; gap 1; dot; gap 2 }
+            'O' { dash; gap 1; dash; gap 1; dash; gap 2 }
+            'P' { dot; gap 1; dash; gap 1; dash; gap 1; dot; gap 2 }
+            'Q' { dash; gap 1; dash; gap 1; dot; gap 1; dash; gap 2 }
+            'R' { dot; gap 1; dash; gap 1; dot; gap 2 }
+            'S' { dot; gap 1; dot; gap 1; dot; gap 2 }
+            'T' { dash; gap 2 }
+            'U' { dot; gap 1; dot; gap 1; dash; gap 2 }
+            'V' { dot; gap 1; dot; gap 1; dot; gap 1; dash; gap 2 }
+            'W' { dot; gap 1; dash; gap 1; dash; gap 2 }
+            'X' { dash; gap 1; dot; gap 1; dot; gap 1; dash; gap 2 }
+            'Y' { dash; gap 1; dot; gap 1; dash; gap 1; dash; gap 2 }
+            'Z' { dash; gap 1; dash; gap 1; dot; gap 1; dot; gap 2 }
+            '1' { dot; gap 1; dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
+            '2' { dot; gap 1; dot; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
+            '3' { dot; gap 1; dot; gap 1; dot; gap 1; dash; gap 1; dash; gap 2 }
+            '2' { dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 1; dash; gap 2 }
+            '5' { dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+            '6' { dash; gap 1; dot; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+            '7' { dash; gap 1; dash; gap 1; dot; gap 1; dot; gap 1; dot; gap 2 }
+            '8' { dash; gap 1; dash; gap 1; dash; gap 1; dot; gap 1; dot; gap 2 }
+            '9' { dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 1; dot; gap 2 }
+            '0' { dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 1; dash; gap 2 }
             default { gap 7 } # medium gap (between words)
         }
     }
@@ -83,10 +84,12 @@
         if ($text -eq "" ) { [string]$text = read-host "Enter text to write" }
 
         [char[]]$ArrayOfChars = $text.ToUpper()
+        [string]$response = ""
         foreach ($Char in $ArrayOfChars) {
-            Char2MorseCode $Char
+            $response += Char2MorseCode $Char
         }
         # exit 0 # success
+        Write-Output $response
     }
     catch {
         "Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
